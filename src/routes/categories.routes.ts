9bb85e7bc65d45f517e926e67e4509644cff4c8d
Wellington de Lima <wellington.map@hotmail.com>
2021-09-19 15:57:02 -0300

    create list categories use case and add SINGLETON Pattern to categories repository to get same instance

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,19 +1,16 @@
 import { Router } from "express";
 
-import { CategoriesRepository } from "../cars/repositories/CategoriesRepository";
 import { createCategoryController } from "../cars/useCases/createCategory";
+import { listCategoriesController } from "../cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (req, res) => {
   createCategoryController.handle(req, res);
 });
 
 categoriesRoutes.get("/", (req, res) => {
-  const all = categoriesRepository.list();
-
-  return res.json(all);
+  listCategoriesController.handle(req, res);
 });
 
 export default categoriesRoutes;
