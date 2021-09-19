dbfe3d7fda754047021f1ea165b766941d906674
Wellington de Lima <wellington.map@hotmail.com>
2021-09-19 15:38:09 -0300

    refactoring create category to use cases and create controller

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,19 +1,13 @@
 import { Router } from "express";
 
-import { CategoriesRepository } from "../repositories/CategoriesRepository";
-import { CreateCategoryService } from "../services/CreateCategoryService";
+import { CategoriesRepository } from "../cars/repositories/CategoriesRepository";
+import { createCategoryController } from "../cars/useCases/createCategory";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (req, res) => {
-  const { name, description } = req.body;
-
-  const createCategoryService = new CreateCategoryService(categoriesRepository);
-
-  createCategoryService.execute({ name, description });
-
-  return res.status(201).send();
+  createCategoryController.handle(req, res);
 });
 
 categoriesRoutes.get("/", (req, res) => {
