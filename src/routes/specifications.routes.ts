aa46d93cba7af65f9078a56a11b3c29ca3f63f18
Wellington de Lima <wellington.map@hotmail.com>
2021-10-07 21:45:47 -0300

    create ensure authenticated middleware

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { CreateSpecificationController } from "../modules/cars/useCases/createSpecification/CreateSpecificationController";
 
 const specificationsRoutes = Router();
+
+specificationsRoutes.use(ensureAuthenticated);
 
 const createSpecificationController = new CreateSpecificationController();
 specificationsRoutes.post("/", createSpecificationController.handle);
