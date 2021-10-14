da3d3c9be166ff0716b2b6c79d5ec8c2be0bca8a
Wellington de Lima <wellington.map@hotmail.com>
2021-10-13 22:05:13 -0300

    create user in memory repo and authenticate user use case test

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -0,0 +1,72 @@
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { UsersRepositoryInMemory } from "../../repositories/in-memory/UsersRepositoryInMemory";
+import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
+import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
+
+let authenticateUserUseCase: AuthenticateUserUseCase;
+let usersRepositoryInMemory: UsersRepositoryInMemory;
+let createUserUseCase: CreateUserUseCase;
+
+describe("Authenticate User", () => {
+  beforeEach(() => {
+    usersRepositoryInMemory = new UsersRepositoryInMemory();
+    authenticateUserUseCase = new AuthenticateUserUseCase(
+      usersRepositoryInMemory
+    );
+    createUserUseCase = new CreateUserUseCase(usersRepositoryInMemory);
+  });
+
+  it("should be able to authenticate an user", async () => {
+    const user: ICreateUserDTO = {
+      driver_license: "000123",
+      email: "johndoe@test.com",
+      password: "1234",
+      name: "john doe",
+    };
+
+    await createUserUseCase.execute(user);
+
+    const result = await authenticateUserUseCase.execute({
+      email: user.email,
+      password: user.password,
+    });
+
+    expect(result).toHaveProperty("token");
+    expect(result).toMatchObject({
+      user: { name: user.name, email: user.email },
+    });
+  });
+
+  it("should not be able to authenticate an non existent user", async () => {
+    expect(async () => {
+      await authenticateUserUseCase.execute({
+        email: "false@test.com",
+        password: "1234",
+      });
+    }).rejects.toMatchObject({
+      message: "Email or password incorrect!",
+      statusCode: 400,
+    });
+  });
+
+  it("should not be able to authenticate with incorrect password", async () => {
+    expect(async () => {
+      const user: ICreateUserDTO = {
+        driver_license: "000123",
+        email: "johndoe@test.com",
+        password: "1234",
+        name: "john doe",
+      };
+
+      await createUserUseCase.execute(user);
+
+      await authenticateUserUseCase.execute({
+        email: user.email,
+        password: "incorrectPassword",
+      });
+    }).rejects.toMatchObject({
+      message: "Email or password incorrect!",
+      statusCode: 400,
+    });
+  });
+});
