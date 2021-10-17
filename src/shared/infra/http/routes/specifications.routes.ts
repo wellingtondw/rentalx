d8bfa1fe706f9580ab070d8ef82cda6442d315fe
Wellington de Lima <wellington.map@hotmail.com>
2021-10-17 17:57:21 -0300

    refactoring layers in app

--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
 
-import { ensureAuthenticated } from "@middlewares/ensureAuthenticated";
 import { CreateSpecificationController } from "@modules/cars/useCases/createSpecification/CreateSpecificationController";
+
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const specificationsRoutes = Router();
 
